c38a92cf19a2deab449709b42dd5fc818284f30f
Brendan Leal <brendan.leal@outlook.com>
2022-03-20 22:07:16 -0700

    Group terraform logic into it's own directory
    Co-authored-by: Michael Fatigati <fatigati.michael@gmail.com>
    Co-authored-by: Samantha Lipari <samanthalipari@gmail.com>
    Co-authored-by: Drew Sessler <drew.sessler@gmail.com>

---
+++ terraform/main.tf
@@ -0,0 +1,33 @@
+### main.tf
+# Specify the provider and access details
+provider "aws" {
+  # use environment variables to specify AWS access and secret access keys
+  region     = var.aws_region
+}
+resource "aws_instance" "manager1" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = ["${aws_security_group.sgswarm.id}", "${aws_security_group.allow_http.id}", "${aws_security_group.ssh.id}", "${aws_security_group.traefik_dashboard.id}"]
+  tags = {
+    Name = "tf manager 1"
+  }
+}
+resource "aws_instance" "worker1" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = ["${aws_security_group.sgswarm.id}", "${aws_security_group.ssh.id}"]
+  tags = {
+    Name = "tf worker 1"
+  }
+}
+resource "aws_instance" "worker2" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = ["${aws_security_group.sgswarm.id}", "${aws_security_group.ssh.id}"]
+  tags = {
+    Name = "tf worker 2"
+  }
+}
