762569b4c9247abb76604d865015c2e12442e242
Brendan Leal <leal.brendan@gmail.com>
2022-03-24 01:43:21 -0700

    The init command successfully preps server for API
    Co-authored-by: Michael Fatigati <fatigati.michael@gmail.com>
    Co-authored-by: Samantha Lipari <samanthalipari@gmail.com>
    Co-authored-by: Drew Sessler <drew.sessler@gmail.com>

--- sentinel/src/commands/init.ts
+++ sentinel/src/commands/init.ts
@@ -1,9 +1,9 @@
 /* eslint-disable object-curly-spacing */
 import { Command } from '@oclif/core'
-const inquirer = require('inquirer')
+// const inquirer = require('inquirer')
 // const process = require("process");
-const childProcess = require("child_process");
-// const { spawn } = require("child_process");
+const childProcess = require('child_process')
+// const { spawn } = require("child_process")
 
 export default class Init extends Command {
   static description = 'Initialize Sentinel'
@@ -20,20 +20,28 @@ export default class Init extends Command {
     try {
       this.log('Initializing cloud infrastructure. Please wait this may take a few minutes.')
 
-      console.log(await execute('cd ./terraform && terraform init'))
+      console.log(await execute('cd ./utils/terraform && terraform init'))
 
       this.log('Provisioning resources. Please wait a little longer.')
 
-      console.log(await execute('cd ./terraform && terraform apply -auto-approve'))
+      console.log(await execute('cd ./utils/terraform && terraform apply -auto-approve'))
+
+      // Give AWS a few seconds to catch up before running ansible plays
+      console.log(await execute('sleep 3.5'))
+
+      this.log('The Sentinel server has successfully started up, but we need your permission to SSH into it to make final adjustments\n')
+
+      console.log(await execute('cd ./utils/ansible && ansible-playbook -i ./inventory/hosts init.yml --fork 1'))
 
       // TODO: Secure copy the host file to ec2 at /home/ec2-user/sentinel-api
+
 
       this.log('Successfully initializing cloud infrastructure')
 
       // TODO: prompt user to create password that will be used to authenticate subsequent requests
       // POST to our api endpoint
-    } catch (err) {
-      console.log(err)
+    } catch (error) {
+      console.log(error)
       this.error('An error ocurred while initializing your infrastructure.')
     }
   }
